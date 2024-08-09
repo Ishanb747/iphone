0e7cd6f96dc8d926b2532620a3ad7dc0a4ff8e1d
Ishan Bhattacharya <generatedelectricity@gmail.com>
2024-08-09 22:05:14 +0530

    finished iphone project, review code

---
+++ src/App.jsx
@@ -0,0 +1,31 @@
+import { useState } from 'react'
+
+import Hero from './components/Hero'
+import Navbar from './components/Navbar'
+import Highlights from './components/Highlights'
+import Model from './components/Model'
+import Features from './components/features'
+import Footer from './components/Footer'
+import HowItWorks from './components/HowItWorks'
+
+const  App = () => {
+  const [count, setCount] = useState(0)
+
+  return (
+    <main className="bg-black">
+      
+      <Navbar/>
+      <Hero/>
+      <Highlights/>
+      <Model/>
+      <Features/>
+      <HowItWorks/>
+      <Footer/>
+
+
+
+    </main>
+)
+}
+
+export default App
